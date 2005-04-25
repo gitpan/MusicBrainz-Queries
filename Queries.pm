1ff4547207c4454521d7658dce0b665aeece79eb
Sander van Zoest <svanzoest@cpan.org>
2005-04-25 11:09:21 +0000

    Import of SVANZOEST/MusicBrainz-Queries-0.08 from CPAN.
    gitpan-cpan-distribution: MusicBrainz-Queries
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         SVANZOEST/MusicBrainz-Queries-0.08.tar.gz
    gitpan-cpan-author:       SVANZOEST
    gitpan-cpan-maturity:     released

--- Queries.pm
+++ Queries.pm
@@ -1,6 +1,6 @@
 package MusicBrainz::Queries;
 
-# $Id: Queries.pm,v 1.7 2004/06/17 22:42:22 sander Exp $
+# $Id: Queries.pm,v 1.8 2005/04/25 10:30:00 sander Exp $
 
 use 5.006_001; 
 use strict;
@@ -26,7 +26,6 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 	MBE_AlbumGetAlbumStatus
 	MBE_AlbumGetAlbumType
 	MBE_AlbumGetAmazonAsin
-	MBE_AlbumGetAmazonCoverartURL
 	MBE_AlbumGetArtistId
 	MBE_AlbumGetArtistName
 	MBE_AlbumGetArtistSortName
@@ -121,7 +120,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw();
 
-our $VERSION = do { my @r = (q$Revision: 1.7 $ =~ /\d+/g); $r[0]--;sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker  
+our $VERSION = do { my @r = (q$Revision: 1.8 $ =~ /\d+/g); $r[0]--;sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker  
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
@@ -205,10 +204,6 @@ Return the release type of the currently selected Album.
 =item MBE_AlbumGetAmazonAsin
 
 Return the Amazon.com ASIN for the currently selected Album.
-
-=item MBE_AlbumGetAmazonCoverartURL
-
-Return the Amazon.com URL for the cover art of the currently selected Album.
 
 =item MBE_AlbumGetArtistId
 
@@ -695,7 +690,7 @@ Sander van Zoest, E<lt>svanzoest@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003 by Alexander van Zoest
+Copyright 2003-2005 by Alexander van Zoest
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
